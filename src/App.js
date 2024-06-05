b84765aaf354f900bb69f392f90784df106e4113
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-06-05 08:00:26 -0700

    resume

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+function container() {
+    return (
+        <div id="container">
+            <div id="maindiv">
+                <div id="img">
+                    <img src="https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Logo" />
+                </div>
+                <div id="text">
+                    <h3>
+                        ANNE
+                    </h3>
+                    <h1>
+                        ROBERTSON
+                    </h1>
+                    <h5>
+                        WEB DESIGNER
+                    </h5>
+                    <div id="yellowdiv">
+                    </div>
+                    <div id="jobs">
+                        <h2>
+                            JOB EXPERIENCE
+                        </h2>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                        <span>2019-Present</span>
+                        <span> Enter Your JOb Possition Here</span>
+                        <span>Company Name</span>
+                    </div>
+                    <div id="yellow2">
+
+                    </div>
+                    <p id="yelpra">
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipiscing
+                        Proin vel arcu eget sem
+                        scelerisque vehicula
+                        aliquet tincidunt enim,
+                        vel vehicula nulla
+                        vel vehicula nulla.</p>
+
+                    <div id="yellow3">
+                        <span>2016-2019</span>
+                        <span> Enter Your JOb Possition Here</span>
+                        <span>Company Name</span>
+                    </div>
+
+                    <div id="yellow2">
+                    </div>
+                    <p id="yelpra">
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipiscing
+                        Proin vel arcu eget sem
+                        scelerisque vehicula
+                        aliquet tincidunt enim,
+                        vel vehicula nulla
+                        vel vehicula nulla.</p>
+
+
+                    <div id="skills">
+                        <h2>
+                            SKILLS
+                        </h2>
+                        <div id="ski1">
+                            <p>Photoshop</p>
+                            <p>lllustrator</p>
+                            <p>lndesign</p>
+                            <p>lndesign</p>
+                        </div>
+                        <div id="ski2">
+                            <p>After lffect</p>
+                            <p>Adobe XD</p>
+                            <p>Powerpoint</p>
+                        </div>
+                    </div>
+                    <div id="interests">
+                        <h2>
+                            INTERESTS
+                        </h2>
+                        <div id="int1">
+                            <ul>
+                                <li>Singing</li>
+                                <li>Traveling</li>
+                                <li>Skekching</li>
+                                <li>Swimming</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div id="section1">
+                    <div id="contact">
+                        <h2>
+                            CONTACT
+                        </h2>
+                        <p>United States</p>
+                        <p>+0123 456 7890</p>
+                        <p>ur@name.com</p>
+                        <p>www.urwebsitename.com</p>
+                    </div>
+                    <div id="education">
+                        <h2>EDUCATION</h2>
+                        <h5>Creative Graphics Art</h5>
+                        <p>University</p>
+                        <p>2019-2020</p>
+
+                        <h5>Bachelor Of Science</h5>
+                        <p>University  Name Here</p>
+                        <p>2015-2019</p>
+
+                    </div>
+                    <div id="refernce">
+                        <h2>REFERNCE</h2>
+                        <div id="yellow4"></div>
+                        <h6>M Devid Parker</h6>
+                        <p>Senior Web Designer</p>
+                        <p>Company Name</p>
+                        <div id="yellow4"></div>
+                        <h6>M Devid Parker</h6>
+                        <p>Senior Web Designer</p>
+                        <p>Company Name</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+
+export default container;
